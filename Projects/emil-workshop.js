20451dfa9efd3baa1420563e81a8a9bfb5d24c95
EN-netizen <emilnis@dtu.dk>
2025-04-29 14:42:09 +0200

    Create emil-workshop.js

---
+++ Projects/emil-workshop.js
@@ -0,0 +1,37 @@
+//Project tile
+const noteData = {
+  title: "WIP seminar by Emil Nissen",
+  date: "30.04.2025", // Use this date format
+  image: "Assets/Test.png", // Put your image in the Assets folder
+  preview: "A seminar discussing a theoretical merger between STS and human geography",
+  researchTopics: [
+    "Topic 1",
+    "Topic 2",
+    "Topic 3"
+  ],
+  objectives: "What you want to achieve with this project",
+  methodology: "How you plan to do it or how you did it",
+  lessons: [
+    "What you learned 1",
+    "What you learned 2",
+    "What you learned 3"
+  ],
+  collaborators: [
+    "Person 1",
+    "Person 2",
+    "Person 3"
+  ],
+
+// Add your links
+projectLink: "https://www.linkedin.com/feed/", // Source link 1
+projectLinkText: "Framework Documentation", // Button text 1
+publicationLink: "https://www.linkedin.com/feed/", // Source link 2
+publicationLinkText: "Download Guidelines", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+  sectionTitles: {
+    topics: "Research Areas",
+    lessons: "Key Learnings",
+    collaborators: "Team Members"
+  }
+};
