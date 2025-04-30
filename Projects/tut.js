c5a3d12e15b6bca9da3d1ccd134d2ccbbd31d9de
apuunDTU <apuun@dtu.dk>
2025-04-30 16:46:34 +0200

    Update tut.js

--- Projects/tut.js
+++ Projects/tut.js
@@ -6,12 +6,12 @@ const noteData = {
   title: "TUT-TEXT UNIT TOOL",
   date: "02.10.2024", // Use this date format
   image: "Assets/Tut.png", // Put your image in the Assets folder
-  preview: "<p>The past decade has brought a data landscape vastly different from what many of us grew up with. Most of the world’s data now exists in unstructured forms: meetings are transcribed, people share thoughts on social media, students post images of what matters to them, and researchers collect ethnographic data on a larger scale.</p>"
-  "<p>Data is no longer just numbers but spans formats like text, images, and audio files. We are developing a tool that provides an interface to navigate these unstructured data formats in new ways. Instead of relying on predefined categories, this tool creates an archive focused on similarities, enabling exploratory analysis.</p>
-  ,
+  preview: `
+    <p>The past decade has brought a data landscape vastly different from what many of us grew up with. Most of the world’s data now exists in unstructured forms: meetings are transcribed, people share thoughts on social media, students post images of what matters to them, and researchers collect ethnographic data on a larger scale.</p>
+    <p>Data is no longer just numbers but spans formats like text, images, and audio files. We are developing a tool that provides an interface to navigate these unstructured data formats in new ways. Instead of relying on predefined categories, this tool creates an archive focused on similarities, enabling exploratory analysis.</p>
+  `,
 
   // right column
-  
   researchTopics: [
     "Computational Anthropology",
     "Digital tools for text Analysis ",
