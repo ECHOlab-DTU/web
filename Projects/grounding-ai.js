2d8f916a34f0b5e8ece43b2bd3c9ed4144a753ce
apuunDTU <apuun@dtu.dk>
2025-04-30 10:26:11 +0200

    Create grounding-ai.js

---
+++ Projects/grounding-ai.js
@@ -0,0 +1,44 @@
+//Grounding AI
+const noteData = {
+  title: "GROUNDING AI - THE AI ATLAS OF AI",
+  date: "11.04.2025", // Use this date format
+  image: "Assets/grounding-ai.png", // Put your image in the Assets folder 
+  preview: "<p> News media articles often portray AI as either “good” or “bad”. We read stories where AI is often “hype” or something we should “fear.” But when we pay attention to what AI research is doing, debates about algorithmic developments are less polarizing and clear cut. Algorithms are addressing issues that remain unnoticed by the public. Rather than oversimplifying AI, this project seeks to invite the public to reflect on and engage with the broader impacts of algorithms.</p>" 
+    "<p>Drawing from the research project “Beyond AI Controversy – What Are Algorithms Doing in Science?,” we are co-creating an interactive exhibition with data physicalization, user journeys, and chatbots as companions. Our goal is to make complex scientific networks relatable, providing a space for exploration and reflection without increasing barriers between science and the public. </p>",
+  researchTopics: [
+    "Public Engagement",
+    "Controversy mapping",
+    "Data visualisation & Data physicalisation",
+    "LLMs for social research"
+  ],
+  objectives: "Experiment with methodologies for engaging the public with large scientific literature corpora.",
+  methodology: [
+    "Exhibition building",
+    "LLM’s for public Engagement",
+    "Digital Methods",
+  ],
+  lessons: [
+    "Interactive website for navigating the visualisation",
+    "A printed floor data map (10x10m) for physicial exploration",
+    "Exhbition building at the Danish Technical Museum"
+    "Public Engagement Exercise in Ballerup Library"
+  ],
+  collaborators: [
+    "Matilde Ficozzi and Mathieu Jacomy - AAU, TANTLab",
+    "Danish Technical Museum",
+    "Dario Rodigherio - Groningen University"
+  ],
+
+// Add your links
+projectLink: "https://grounding-ai.github.io/web-application/#/map", // Source link 1
+projectLinkText: "Oficial Website", // Button text 1
+publicationLink: "https://www.linkedin.com/posts/mathieu-jacomy-a38a552a_step-on-ai-trample-our-giant-visualization-activity-7317871411140718596-N5A3?utm_source=share&utm_medium=member_desktop&rcm=ACoAACaljWgBKsAEU9qml-3Ue7nn-e8ibizgaIk", // Source link 2
+publicationLinkText: "LinkedIn", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+  sectionTitles: {
+    topics: "Research Areas",
+    lessons: "Scope and Deliverables",
+    collaborators: "Team Members"
+  }
+};
