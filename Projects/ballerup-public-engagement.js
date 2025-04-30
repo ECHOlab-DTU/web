9ae4f9fc91340d5c1b44edd93d604014128901c4
Sarah <sarfe@dtu.dk>
2025-04-30 10:20:02 +0200

    Create ballerup-public-engagement.js

---
+++ Projects/ballerup-public-engagement.js
@@ -0,0 +1,45 @@
+//Project tile
+const noteData = {
+  title: "Exhbiting a large-scale data visualization - A public engagement exercise",
+  date: "25.01.2025", // Use this date format
+  image: "Assets/Test.png", // Put your image in the Assets folder
+  preview: "The event at Ballerup Library was part of the “Grounding AI” project, a research collaboration between several universities which materializes in a 100-squaremeter data physicalization, visualizing scientific research featuring algorithms and thereby their diverse roles in science. As an exhibition piece, the large-sale data visualization explores the intersection of aesthetics, science, and AI-generated visualizations.Stylized as a “map”, the piece shows 2,015,03 scientific articles on algorithms that visitors can walk on and explore the space of algorithms in scientific literature. The aim with exhibiting the map is to engage publics in dialogue about the role algorithms play in all areas of life, their omnipresence and mundaneness, that is seldom featured in public debate about AI. Presenting the map at Ballerup Library was the first test of offering a space presentation, discussion, and questions about the piece and the research behind it.",
+  researchTopics: [
+    "Data Physicalization for public engagement",
+    "large-scale data visualizations",
+    "Algorithms in Scientifuc Literature"
+  ],
+  objectives: [
+    "testing the 'Grounding Ai' data visualization",
+    "testing exhibition building",
+    "prompting an audience to discussion and reflection about Artificial Intelligence and Algorithms through large-scale data visualizations"
+    ],  
+  methodology: [
+    "exhibiting the floor map and description posters",
+    "presentation of the 'Groudnin AI' project",
+    "Q&A session with the research team, led by Professor Anders Munk"
+    ],
+  lessons: "The event lasted one evening, guiding participants through the project and the exhibition piece. In the future, the map as well as additional exhibition pieces will be presented in a longer term exhibition at the Danish Technical Museum in Helsingør. Information regarding the project and exhibition can be found on the Grounding AI website and the museum’s website.",
+  collaborators: [
+    "TANT Lab (Aalborg University)",
+    "University of Groningen (NL)",
+    "ADD - Algorithms, Data & Democracy",
+    "ECHO Lab - The Observatory for Human Centered Engineering (DTU)",
+    "Ballerup Library"
+  ],
+
+// Add your links
+projectLink: "https://grounding-ai.github.io", // Source link 1
+projectLinkText: "Project Website", // Button text 1
+publicationLink: "https://bsky.app/profile/echodtu.bsky.social/post/3lge3cuza322q", // Source link 2
+publicationLinkText: "Event Post", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+  sectionTitles: {
+    researchTopics: "Research Topics",
+    objectives: "Objectives",
+    methodology: "methodology",
+    lessons: "scope and future exhibitions",
+    collaborators: "collaborators"
+  }
+};
