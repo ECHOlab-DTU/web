b89f900ed5a8a85a83bd99973f8fce2701044502
apuunDTU <apuun@dtu.dk>
2025-04-30 11:07:48 +0200

    Update Experimenting-with-large-edata.js

--- Projects/Experimenting-with-large-edata.js
+++ Projects/Experimenting-with-large-edata.js
@@ -1,37 +1,50 @@
 //EXPERIMENTING WITH LARGE ETHNOGRAPHIC DATA
 const noteData = {
-  title: "YOUR PROJECT TITLE",
-  date: "DD.MM.YYYY", // Use this date format
+  title: "Experimenting with large ethnographic data",
+  date: "02.10.2024", // Use this date format
   image: "Assets/Test.png", // Put your image in the Assets folder
-  preview: "A brief description of your project...",
+  preview: “<p>Computational methods have opened up new possibilities for collecting and analyzing large quantities of ethnographic data, including interviews, comments, articles, and abstracts.</p>" 
+  “<p> Applying tools like sentence transformers and embeddings, we can cluster and group texts with similar meanings and visualize them, revealing emerging patterns in the data. With these methods, we use a cross-disciplinary approach between computational and social sciences makes it possible to use existing qualitative text data in new ways.</p>"  
+  “<p> This workshop focused on the practical application of computational tools to ethnographic data. In this experiment participants engaged in a guided exercise using interviews from their individual research projects. The session concluded with discussions and reflections on how to integrate these methodologies into ethnographic research moving forward.</p>"
+,
   researchTopics: [
-    "Topic 1",
-    "Topic 2",
-    "Topic 3"
+    "Computational Anthropology",
+    "Qualitative Data Analysis",
+    "Experimental Ethnography",
+    "Data Visualization",
   ],
-  objectives: "What you want to achieve with this project",
-  methodology: "How you plan to do it or how you did it",
+  objectives: [
+    "Gain familiarity with computational methods through experimenting with ethnographic data",
+    "Learn about new tools & methodologies to analyze large volumes of text for research purposes",
+    "Analyze and use qualitative data for STS reserarch in new ways",
+  ],
+  methodology: [
+    "Prepare data files by creating a spreadsheet with text data from interviews",
+    "Downloading and setting up relevant software and tools",
+    "Sentence Embeddings with Sentence Transformers",
+    "Visualizing Sentence Embeddings with DataMapPlot and / or CorText",
+  ],
   lessons: [
-    "What you learned 1",
-    "What you learned 2",
-    "What you learned 3"
+    "Introduction to computational methods and tools for working with large bodies of textual data",
+    "Hands-on experience and experimenting with tools for clustering, analyzing, and visualizing textual data",
+    "Discussions & reflections on  how digital methods can be implemented in future research projects"
   ],
   collaborators: [
-    "Person 1",
-    "Person 2",
-    "Person 3"
+    "Johan Irving Søltoft (DTU)",
+    "Anders Kristian Munk (DTU)",
+    "Britt Ross Winthereik (DTU)"
   ],
 
 // Add your links
-projectLink: "https://www.linkedin.com/feed/", // Source link 1
-projectLinkText: "Framework Documentation", // Button text 1
-publicationLink: "https://www.linkedin.com/feed/", // Source link 2
-publicationLinkText: "Download Guidelines", // Button text 2
+projectLink: "https://github.com/ECHOlab-DTU/Experimenting-with-large-ethnographic-data-workshop", // Source link 1
+projectLinkText: "Github", // Button text 1
+publicationLink: "https://www.linkedin.com/posts/echodtu_%F0%9D%90%84%F0%9D%90%B1%F0%9D%90%A9%F0%9D%90%9E%F0%9D%90%AB%F0%9D%90%A2%F0%9D%90%A6%F0%9D%90%9E%F0%9D%90%A7%F0%9D%90%AD%F0%9D%90%A2%F0%9D%90%A7%F0%9D%90%A0-%F0%9D%90%B0%F0%9D%90%A2%F0%9D%90%AD%F0%9D%90%A1-%F0%9D%90%8B-activity-7269288295212220417-_Z24?utm_source=share&utm_medium=member_desktop&rcm=ACoAACaljWgBKsAEU9qml-3Ue7nn-e8ibizgaIk", // Source link 2
+publicationLinkText: "LinkedIn", // Button text 2
 
 // OPTIONAL: Change section titles (right column)
   sectionTitles: {
-    topics: "Research Areas",
-    lessons: "Key Learnings",
+    lessons: "Scope",
+    methodology: "how to do it",
     collaborators: "Team Members"
   }
 };
