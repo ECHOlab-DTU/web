8e2898f59b8eebf8276c55cf729dfff43fa5e0b6
apuunDTU <apuun@dtu.dk>
2025-04-30 10:36:24 +0200

    Create tut.js

---
+++ Projects/tut.js
@@ -0,0 +1,50 @@
+//TUT - TEXT  UNIT TOOL 
+const noteData = {
+  
+//left column
+  
+  title: "TUT-TEXT UNIT TOOL",
+  date: "02.10.2024", // Use this date format
+  image: "Assets/Tut.png", // Put your image in the Assets folder
+  preview: "<p>The past decade has brought a data landscape vastly different from what many of us grew up with. Most of the world’s data now exists in unstructured forms: meetings are transcribed, people share thoughts on social media, students post images of what matters to them, and researchers collect ethnographic data on a larger scale.</p>"
+  "<p>Data is no longer just numbers but spans formats like text, images, and audio files. We are developing a tool that provides an interface to navigate these unstructured data formats in new ways. Instead of relying on predefined categories, this tool creates an archive focused on similarities, enabling exploratory analysis.</p>
+  ,
+
+  // right column
+  
+  researchTopics: [
+    "Computational Anthropology",
+    "Digital tools for text Analysis ",
+    "Archiving",
+    "Digital Methods",
+  ],
+  objectives: "We aim to develop a tool that can be used across industry and academia to help navigate and analyze large qualitative corpora.
+",
+  methodology: "We used Large language models to calculate semantic similarity between text units, and we have developed a navigation system that we call “Navigation for Serendipity.”
+",
+  lessons: [
+    "Conducting workshops with different research groups on how this tool could be used in their research",
+    "Presenting the tool at ICWSM"
+  ],
+  collaborators: [
+    "Will & Agency",
+    "MASSHINE (Aalborg University)",
+    "TANT Lab (Aalborg University)",
+    "Agora",
+  
+  ],
+
+// LINKS
+projectLink: "https://www.linkedin.com/feed/", // Source link 1
+projectLinkText: "Framework Documentation", // Button text 1
+publicationLink: "https://www.linkedin.com/feed/", // Source link 2
+publicationLinkText: "Download Guidelines", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+  sectionTitles: {
+    topics: "Research Areas",
+    objectives: "objective",
+    lessons: "Scope and Deliverables",
+    collaborators: "Team Members"
+  }
+};
