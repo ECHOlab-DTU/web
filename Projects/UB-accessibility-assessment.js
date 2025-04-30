64d32039b24996b8409e59e7e9bcb583255381eb
apuunDTU <apuun@dtu.dk>
2025-04-30 11:39:51 +0200

    Create UB-accessibility-assessment.js

---
+++ Projects/UB-accessibility-assessment.js
@@ -0,0 +1,54 @@
+//Web Accessibility Assessment oF Mobile Ethnography  Tools - The Urban Belonging Photo App
+const noteData = {
+  title: "Web Accessibility Assessment oF The Urban Belonging App",
+  date: "01.11.2024", // Use this date format
+  image: "Assets/Test.png", // Put your image in the Assets folder
+  preview: `
+    <p>The workshop was part of a project focused on the <strong>Accessibility Assessment and further development</strong> of the <strong>Urban Belonging Photo App</strong>.</p>
+    <p>The <strong>open-source toolkit</strong> was developed by a group of researchers and architects as part of the Urban Belonging project. It was initially used in research conducted between 2020 and 2023. Upon successful completion of that phase, it was decided to continue developing the app further.</p>
+    <p>Since then, the app has been <strong>maintained by developer Iain Kettles</strong> and used in various participatory projects.</p>
+    <p>At <strong>ECHO Lab</strong>, the app underwent a <strong>Web Accessibility Assessment</strong> conducted by two digital accessibility consultants. During a one-day workshop, they shared their methods, insights, and general reflections on inclusive digital participatory approaches.</p>
+  `,
+
+// right column
+  researchTopics: [
+    "Inclusive Participatory Research Design",
+    "Tools for Participatory Research",
+    "Web Accessibility-App development",
+    "Disability-led design"
+  ],
+
+  objectives: [
+    "Raise awareness about the importance of accessible digital tools in participatory research",
+    "Demonstrate practical tools and methods for conducting web accessibility assessments",
+    "Critically evaluate participants’ own research tools and projects through an accessibility lens",
+    "Co-create a prioritized list of accessibility improvement requirements for the Urban Belonging Photo App"
+  ],
+  methodology: [
+    "Introduction to Web Accessibility guidelines and tools by Anne Thyme",
+    "Accessibility Assessment of the Urban Belonging Photo App by Stein Erik with assistive tech",
+    "Discussion and Q&A with the digital accessiibility consultants",
+  ],
+  lessons: [
+    "Web Accessibility testing and demonstration",
+    "List of accessibility requirements for further app development"
+  ],
+  
+  collaborators: [
+    "Stein Erik Skotsterra (UseIT)",
+    "Anne Thyme (UseIT)",
+    "Sarah Feldes, Ainoa Pubill and Barbara Nino Carreras"
+  ],
+
+// Add your links
+projectLink: "https://github.com/ECHOlab-DTU/UB-App-Accessibility-Development", // Source link 1
+projectLinkText: "Github", // Button text 1
+publicationLink: "https://urbanbelonging.com", // Source link 2
+publicationLinkText: "Urban Belonging Project", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+  sectionTitles: {
+    lessons: "Scope and deliverables",
+    collaborators: "Collaborators"
+  }
+};
