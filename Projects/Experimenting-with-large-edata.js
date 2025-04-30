28f4036a5c755958f3b5696ef52c90c021665a18
apuunDTU <apuun@dtu.dk>
2025-04-30 11:18:01 +0200

    Update Experimenting-with-large-edata.js

--- Projects/Experimenting-with-large-edata.js
+++ Projects/Experimenting-with-large-edata.js
@@ -2,10 +2,11 @@
 const noteData = {
   title: "Experimenting with large ethnographic data",
   date: "02.10.2024", // Use this date format
-  image: "Assets/Test.png", // Put your image in the Assets folder
-  preview: “<p>Computational methods have opened up new possibilities for collecting and analyzing large quantities of ethnographic data, including interviews, comments, articles, and abstracts.</p>" 
-  “<p> Applying tools like sentence transformers and embeddings, we can cluster and group texts with similar meanings and visualize them, revealing emerging patterns in the data. With these methods, we use a cross-disciplinary approach between computational and social sciences makes it possible to use existing qualitative text data in new ways.</p>"  
-  “<p> This workshop focused on the practical application of computational tools to ethnographic data. In this experiment participants engaged in a guided exercise using interviews from their individual research projects. The session concluded with discussions and reflections on how to integrate these methodologies into ethnographic research moving forward.</p>"
+  image: "Assets/Nolabels.png", // Put your image in the Assets folder
+  preview:
+  "<p>Computational methods have opened up new possibilities for collecting and analyzing large quantities of ethnographic data, including interviews, comments, articles, and abstracts.</p>"
+  "<p>Applying tools like sentence transformers and embeddings, we can cluster and group texts with similar meanings and visualize them, revealing emerging patterns in the data. With these methods, we use a cross-disciplinary approach between computational and social sciences, making it possible to use existing qualitative text data in new ways.</p>"
+  "<p>This workshop focused on the practical application of computational tools to ethnographic data. In this experiment, participants engaged in a guided exercise using interviews from their individual research projects. The session concluded with discussions and reflections on how to integrate these methodologies into ethnographic research moving forward.</p>"
 ,
   researchTopics: [
     "Computational Anthropology",
@@ -39,7 +40,7 @@ const noteData = {
 projectLink: "https://github.com/ECHOlab-DTU/Experimenting-with-large-ethnographic-data-workshop", // Source link 1
 projectLinkText: "Github", // Button text 1
 publicationLink: "https://www.linkedin.com/posts/echodtu_%F0%9D%90%84%F0%9D%90%B1%F0%9D%90%A9%F0%9D%90%9E%F0%9D%90%AB%F0%9D%90%A2%F0%9D%90%A6%F0%9D%90%9E%F0%9D%90%A7%F0%9D%90%AD%F0%9D%90%A2%F0%9D%90%A7%F0%9D%90%A0-%F0%9D%90%B0%F0%9D%90%A2%F0%9D%90%AD%F0%9D%90%A1-%F0%9D%90%8B-activity-7269288295212220417-_Z24?utm_source=share&utm_medium=member_desktop&rcm=ACoAACaljWgBKsAEU9qml-3Ue7nn-e8ibizgaIk", // Source link 2
-publicationLinkText: "LinkedIn", // Button text 2
+publicationLinkText: "Event post", // Button text 2
 
 // OPTIONAL: Change section titles (right column)
   sectionTitles: {
