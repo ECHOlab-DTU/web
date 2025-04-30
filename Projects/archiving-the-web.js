dedf13dfaac0b9c3711767e3c1967f1006eea4cc
Sarah <sarfe@dtu.dk>
2025-04-30 11:18:24 +0200

    Create archiving-the-web.js

---
+++ Projects/archiving-the-web.js
@@ -0,0 +1,47 @@
+//Project tile
+const noteData = {
+  title: "Archiving the web: Methods and Applications",
+  date: "28.01.2025", // Use this date format
+  image: "Assets/Test.png", // Put your image in the Assets folder
+  preview: "<p> The internet has become a key research focus and a valuable tool for studying social phenomena across disciplines. In collaboration with the DigiFront initiative, ECHO Lab organized a session where researchers presented their projects, showcasing how they utilize the web and web archives as methods in their research. Katie McKinnon (KU) introduced the concept of the marginal web and her research on digital traces of young people's online engagement in Canada during the 1990s and 2000s, emphasizing ethical and careful approaches to studying these histories. Niels Bruegger (AU) presented his examination of the differences between analogue and web archives and how these distinctions change archival methodologies. He also shared a resource on web history and archiving which he has been developing over the years. Oscar Corominas (UAB) demonstrated how private-sector marketing tools, such as Ahrefs, can be repurposed for digital methods.  Mathieu Jacomy (AAU) shared methods for scraping the web with LLMs, enabling a more granular approach to traditional web scraping by extracting specific elements of the page.</p>",
+  researchTopics: [
+    "Digital Archives",
+    "Archiving as a method",
+    "Digital Archives as resources",
+    "Leveraging web archives as digital research methods"
+  ],
+  objectives: [
+    "Showcase diverse approaches to studying the web as both object of study and a methodological resource",
+    "demonstrate practical tools for web-based research, including digital marketing tools, and LLMs for web scraping"
+  ],
+  methodology: [
+    "Introduction to data extraction from the web",
+    "archiving for preservation",
+    "marketing tools as digital methods",
+    "LLMs for web scraping"
+  ],
+  lessons: [
+    "presentations of ongoing research projects",
+    "group discussions & reflections",
+    "web history archive <a>[link](http://webhistorie.dk)</a> (Niels Bruegger)"
+  ],
+  collaborators: [
+    "Alexei Tsinovoi - Digi Front (Aarhus University)",
+    "Niels Bruegger - Aarhus University",
+    "Katie McKinni - University of Copenhagen",
+    "Mathieu Jacomy - Aalborg University"
+  ],
+
+// Add your links
+projectLink: "https://www.linkedin.com/feed/", // Source link 1
+projectLinkText: "Framework Documentation", // Button text 1
+publicationLink: "https://www.linkedin.com/feed/", // Source link 2
+publicationLinkText: "Download Guidelines", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+  sectionTitles: {
+    topics: "Research Topics",
+    lessons: "Scope & Resources",
+    collaborators: "collaborators"
+  }
+};
