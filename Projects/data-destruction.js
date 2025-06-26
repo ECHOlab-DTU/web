702a09c7e3bcc6a47141d4e9707aff645e2ae744
Sarah <sarfe@dtu.dk>
2025-06-26 08:54:56 +0200

    Update data-destruction.js

--- Projects/data-destruction.js
+++ Projects/data-destruction.js
@@ -1,1 +1,39 @@
+//Project tile
+const noteData = {
+  title: "Data Destruction Workshop",
+  date: "02.06.2025", // Use this date format
+  image: "Assets/Placeholder.png", // Put your image in the Assets folder
+  preview: "A full-day workshop covering theoreitcal and practical levels of what it means to get rid of data. In collaboration with DALOSS (KU) and Datatilsynet",
 
+//Sections -> Right column 
+  researchTopics: [
+    "Fieldwork in ",
+    "Topic 2",
+    "Topic 3"
+  ],
+  objectives: "What you want to achieve with this project",
+  methodology: "How you plan to do it or how you did it",
+  lessons: [
+    "What you learned 1",
+    "What you learned 2",
+    "What you learned 3"
+  ],
+  collaborators: [
+    "Person 1",
+    "Person 2",
+    "Person 3"
+  ],
+
+// Add your links
+projectLink: "https://www.linkedin.com/feed/", // Source link 1
+projectLinkText: "Framework Documentation", // Button text 1
+publicationLink: "https://www.linkedin.com/feed/", // Source link 2
+publicationLinkText: "Download Guidelines", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+sectionTitles: {
+researchTopics: "Custom title here",
+lessons: "Custom title here",
+collaborators: "Custom title here"
+  }
+};
