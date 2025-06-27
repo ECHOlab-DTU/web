2e614a49249eac9ced3161d33dde43b8105f2fb7
Ainoa Pubill <apuun@dtu.dk>
2025-06-27 11:56:14 +0200

    Create EnergyIsland.js

---
+++ Projects/EnergyIsland.js
@@ -0,0 +1,39 @@
+//Project tile
+const noteData = {
+  title: "Energy Islands. Issue",
+  date: "DD.MM.YYYY", // Use this date format
+  image: "Assets/Placeholder.png", // Put your image in the Assets folder
+  preview: "A brief description of your project...",
+
+//Sections -> Right column 
+  researchTopics: [
+    "Topic 1",
+    "Topic 2",
+    "Topic 3"
+  ],
+  objectives: "What you want to achieve with this project",
+  methodology: "How you plan to do it or how you did it",
+  lessons: [
+    "What you learned 1",
+    "What you learned 2",
+    "What you learned 3"
+  ],
+  collaborators: [
+    "Person 1",
+    "Person 2",
+    "Person 3"
+  ],
+
+// Add your links
+projectLink: "https://www.linkedin.com/feed/", // Source link 1
+projectLinkText: "Framework Documentation", // Button text 1
+publicationLink: "https://www.linkedin.com/feed/", // Source link 2
+publicationLinkText: "Download Guidelines", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+sectionTitles: {
+researchTopics: "Custom title here",
+lessons: "Custom title here",
+collaborators: "Custom title here"
+  }
+};
