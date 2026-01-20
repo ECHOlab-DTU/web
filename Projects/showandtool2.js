04bef10a09dc79458e02949a9bb2ab3e987e3bcc
Johan Irving Sølotft <100198311+Johansoltoft@users.noreply.github.com>
2026-01-20 19:43:01 +0100

    Create showandtool2.js

---
+++ Projects/showandtool2.js
@@ -0,0 +1,34 @@
+//Project tile
+const noteData = {
+  title: "Text Extraction With LLMs. Show&Tool #2",
+  date: "4.12.2025", // Use this date format
+  image: "Assets/showandtool2.jpg", // Put your image in the Assets folder
+  preview: "Working with unstructured text such as interview transcripts or news articles presents challenges and tradeoffs when we want to extract statements made by, and statements about, specific actors. This presentation will first outline the principles of working with text as a datatype by assessing its practical limitations. Then we’ll take a detailed look at transcribed text as a data source and share a best-practice roadmap to statement extraction on GitHub. Through that, we’ll see how different methodological choices affect accuracy and reliability.",
+
+//Sections -> Right column 
+  researchTopics: [
+    "Large language models",
+    "Interview data",
+    "qualitative data",
+  ],
+  objectives: "LLM-supported statement extraction from complex textual data.",
+
+  ],
+  collaborators: [
+    "Guest: Andreas Rattenborg Holm-Hansen, Associate analyst at INVI & BSc in Design & Innovation from DTU.",
+    "Guest: Sofie Burgos-Thorsen, R&D director at INVI."
+  ],
+
+// Add your links
+//projectLink: "https://github.com/ECHOlab-DTU/Energy-Islands-Issue-Atlas", // Source link 1
+//projectLinkText: "Github Repository", // Button text 1
+//publicationLink: "https://www.linkedin.com/feed/", // Source link 2
+//publicationLinkText: "Download Guidelines", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+sectionTitles: {
+researchTopics: "Research areas",
+lessons: "Deliverables",
+collaborators: "Project team"
+  }
+};
