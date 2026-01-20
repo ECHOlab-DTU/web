692b09fa90288dd425f0bc6715a2bc260ee83176
Johan Irving Sølotft <100198311+Johansoltoft@users.noreply.github.com>
2026-01-20 19:51:16 +0100

    Update showandtool2.js

--- Projects/showandtool2.js
+++ Projects/showandtool2.js
@@ -1,6 +1,9 @@
-//Project tile
+//SHOW&TOOL#2 
 const noteData = {
-  title: "Text Extraction With LLMs. Show&Tool #2",
+  
+//left column
+  
+title: "Text Extraction With LLMs. Show&Tool #2",
   date: "4.12.2025", // Use this date format
   image: "Assets/showandtool2.jpg", // Put your image in the Assets folder
   preview: "Working with unstructured text such as interview transcripts or news articles presents challenges and tradeoffs when we want to extract statements made by, and statements about, specific actors. This presentation will first outline the principles of working with text as a datatype by assessing its practical limitations. Then we’ll take a detailed look at transcribed text as a data source and share a best-practice roadmap to statement extraction on GitHub. Through that, we’ll see how different methodological choices affect accuracy and reliability.",
@@ -11,24 +14,30 @@ const noteData = {
     "Interview data",
     "qualitative data",
   ],
-  objectives: "LLM-supported statement extraction from complex textual data.",
-
+  objectives: "We aim to develop a tool that can be used across industry and academia to help navigate and analyze large qualitative corpora."
+//  ,
+ // methodology: "We used Large language models to calculate semantic similarity between text units, and we have developed a navigation system that we call “Navigation for Serendipity."
+//  ,
+ // lessons: [
+ //   "Conducting workshops with different research groups on how this tool could be used in their research"
+ // ],
   ],
   collaborators: [
     "Guest: Andreas Rattenborg Holm-Hansen, Associate analyst at INVI & BSc in Design & Innovation from DTU.",
     "Guest: Sofie Burgos-Thorsen, R&D director at INVI."
   ],
 
-// Add your links
-//projectLink: "https://github.com/ECHOlab-DTU/Energy-Islands-Issue-Atlas", // Source link 1
-//projectLinkText: "Github Repository", // Button text 1
-//publicationLink: "https://www.linkedin.com/feed/", // Source link 2
-//publicationLinkText: "Download Guidelines", // Button text 2
+// LINKS
+//projectLink: "https://tut.man.dtu.dk/", // Source link 1
+//projectLinkText: "Website", // Button text 1
+//publicationLink: "", // Source link 2
+//publicationLinkText: "", // Button text 2
 
 // OPTIONAL: Change section titles (right column)
-sectionTitles: {
-researchTopics: "Research areas",
-lessons: "Deliverables",
-collaborators: "Project team"
+ sectionTitles: {
+researchTopics: "Research Areas",
+lessons: "Scope",
+collaborators: "Facilitators"
   }
 };
+
